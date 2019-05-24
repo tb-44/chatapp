a079ef9e573824424012c410bb1f45759cce7235
Trent Bennett <rehab.rtb@gmail.com>
2019-05-24 14:37:27 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
-import Login from "./components/Login";
-import Groupchat from "./components/Groupchat";
+// import React, { Component } from "react";
+// import { Route, Redirect, Switch } from "react-router-dom";
+// import "./App.css";
+// import Login from "./components/Login";
+// import Groupchat from "./components/Groupchat";
+// import chat from "./lib/chat";
 
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-render() {
-    return (
-      <Switch>
-        <Redirect exact from="/" to="/login" />
-        <Route path="/login" component={Login} />
-        <Route path="/chat" component={Groupchat} />
-      </Switch>
-    );
-  }
-}
-export default App;
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     chat.init();
+//   }
+
+//   render() {
+//     return (
+//       <Switch>
+//         <Redirect exact from="/" to="/login" />
+//         <Route path="/login" component={Login} />
+//         <Route path="/chat" component={Groupchat} />
+//       </Switch>
+//     );
+//   }
+// }
+
+// export default App;
