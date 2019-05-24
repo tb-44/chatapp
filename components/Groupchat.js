c6ad864d146f90a16556dcdeed7c4291be4b49b1
Trent Bennett <rehab.rtb@gmail.com>
2019-05-23 21:30:29 -0600

    first commit

---
+++ components/Groupchat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Groupchat extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+  render() {
+    return <div className="chatWindow" />;
+  }
+}
+export default Groupchat;
---
+++ components/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return ( 
+      <div className="App">
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
+export default Login;
